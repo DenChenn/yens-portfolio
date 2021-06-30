83f1ffad04718cd0911840ad6af2888de9934cf9
SwarzChen <aaaa102234@gmail.com>
2021-06-30 18:37:33 +0800

    :coffin: delete unused code

--- main.js
+++ main.js
@@ -1,84 +1,76 @@
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { BuildWater } from './objects/water'
-import { BuildSky } from './objects/sky';
-import { BuildSun } from './objects/sun';
-import { BuildBoat } from './objects/boat';
-import { BuildBridge } from './objects/bridge';
-import { BuildGate } from './objects/gate';
-import { BuildLantern } from './objects/lantern';
+import { BuildSky } from './objects/sky'
+import { BuildSun } from './objects/sun'
+import { BuildBridge } from './objects/bridge'
+import { BuildGate } from './objects/gate'
+import { BuildLantern } from './objects/lantern'
+import { BuildTemple } from './objects/temple'
+import Boat from './objects/boat'
 
-
-
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 function buildCamera() {
-  const camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 1, 20000);
-  camera.position.set(30, 30, 100);
-  return camera;
+  const camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000,
+  )
+  camera.position.set(30, 30, 100)
+  return camera
 }
 
 //resizing window
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
-window.addEventListener('resize', onWindowResize);
+window.addEventListener('resize', onWindowResize)
 
-const camera = buildCamera();
+const camera = buildCamera()
 
 const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#bg')
-});
+  canvas: document.querySelector('#bg'),
+})
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
 
-renderer.render(scene, camera);
+renderer.render(scene, camera)
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({color: 0x6b0000});
+const pointLight = new THREE.PointLight(0xffffff)
+const ambienLight = new THREE.AmbientLight(0xfffff)
 
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-const pointLight = new THREE.PointLight(0xffffff);
-const ambienLight = new THREE.AmbientLight(0xfffff);
-
-scene.add(pointLight, ambienLight);
+scene.add(pointLight, ambienLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
-scene.add(lightHelper);
+scene.add(lightHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement)
 
-const water = BuildWater(scene);
-const sky = BuildSky();
+const water = BuildWater(scene)
+const sky = BuildSky()
 
 scene.add(water)
 scene.add(sky)
-BuildSun(scene, renderer, sky);
+BuildSun(scene, renderer, sky)
 
-BuildBridge(scene, 20, 20, 0);
-BuildBoat(scene, 50, 20, -2);
-BuildGate(scene, 20, 50, 0);
-BuildLantern(scene, 20, -50, 0);
+BuildBridge(scene, 20, 20, 0)
+const boat = new Boat(scene, 0, -2, 0)
+BuildGate(scene, 20, 50, 0)
+BuildLantern(scene, 20, -50, 0)
+BuildTemple(scene, 50, 50, 0)
 
 function animate() {
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  water.material.uniforms[ 'time' ].value += 1.0 / 60.0;
-  renderer.render(scene, camera);
+  requestAnimationFrame(animate)
+  boat.update()
+  water.material.uniforms['time'].value += 1.0 / 60.0
+  renderer.render(scene, camera)
 }
 
-animate();
-
-
-
+animate()
