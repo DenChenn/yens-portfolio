5fe63faf2baca3ba6456203ff6ccbf4c9d384783
SwarzChen <aaaa102234@gmail.com>
2021-07-01 18:56:47 +0800

    :zap: let boat be an object

--- objects/boat.js
+++ objects/boat.js
@@ -1,7 +1,7 @@
-import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as THREE from 'three'
 import { MAX_VELOCITY, ACCELERATION } from './config'
+import CANNON from 'cannon'
 
 class VelocityModel {
   v = 0
@@ -25,10 +25,10 @@ class VelocityModel {
         this.a = ACCELERATION
       }
       if (keyPress === 'd') {
-        this.theta += Math.PI / 128
+        this.theta += Math.PI / 100
       }
       if (keyPress === 'a') {
-        this.theta -= Math.PI / 128
+        this.theta -= Math.PI / 100
       }
     })
 
@@ -52,30 +52,70 @@ class VelocityModel {
 }
 
 class Boat {
-  object = new THREE.Object3D()
   front = new VelocityModel('w')
   back = new VelocityModel('s')
+  mesh = new THREE.Mesh()
 
-  constructor(scene, posX, posY, posZ) {
+  sphereBody = new CANNON.Body()
+
+  constructor(scene, world, posX, posY, posZ) {
     const loader = new GLTFLoader()
     loader.load('../models/tiny_boat/scene.gltf', (gltf) => {
-      this.object.add(SkeletonUtils.clone(gltf.scene))
-      this.object.position.set(posX, posY, posZ)
-      this.object.scale.set(0.2, 0.2, 0.2)
-      scene.add(this.object)
+      this.mesh = gltf.scene
+      this.mesh.scale.set(0.1, 0.1, 0.1)
+      scene.add(this.mesh)
     })
+
+    let sphereShape = new CANNON.Sphere(1)
+    let sphereCM = new CANNON.Material()
+    this.sphereBody = new CANNON.Body({
+      mass: 5,
+      shape: sphereShape,
+      position: new CANNON.Vec3(0, 10, 0),
+      material: sphereCM,
+    })
+    world.add(this.sphereBody)
+
+    //ground
+    let groundShape = new CANNON.Plane()
+    let groundCM = new CANNON.Material()
+    let groundBody = new CANNON.Body({
+      mass: 0,
+      shape: groundShape,
+      material: groundCM,
+      position: new CANNON.Vec3(0, -2, 0),
+    })
+    groundBody.quaternion.setFromAxisAngle(
+      new CANNON.Vec3(1, 0, 0),
+      -Math.PI / 2,
+    )
+    world.add(groundBody)
+
+    let sphereGroundContact
+    sphereGroundContact = new CANNON.ContactMaterial(groundCM, sphereCM, {
+      friction: 0.5,
+      restitution: 0.7,
+    })
+    world.addContactMaterial(sphereGroundContact)
   }
 
   update() {
     this.front.accelerate()
     this.back.accelerate()
 
-    this.object.position.x += this.front.v * Math.cos(this.front.theta)
-    this.object.position.z += this.front.v * Math.sin(this.front.theta)
-    this.object.position.x -= this.back.v * Math.cos(this.front.theta)
-    this.object.position.z -= this.back.v * Math.sin(this.front.theta)
+    this.sphereBody.position.x -= this.front.v * Math.cos(this.front.theta)
+    this.sphereBody.position.z -= this.front.v * Math.sin(this.front.theta)
+    this.sphereBody.position.x += this.back.v * Math.cos(this.front.theta)
+    this.sphereBody.position.z += this.back.v * Math.sin(this.front.theta)
 
-    this.object.rotation.y = -this.front.theta
+    this.mesh.rotation.y = -this.front.theta
+    this.sphereBody.quaternion.setFromAxisAngle(
+      new CANNON.Vec3(0, 1, 0),
+      this.mesh.rotation.y,
+    )
+
+    this.mesh.position.copy(this.sphereBody.position)
+    this.mesh.quaternion.copy(this.sphereBody.quaternion)
   }
 }
 
