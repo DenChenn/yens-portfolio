065cf0cffbc63dea980e6623dd48272f76ab77dc
SwarzChen <aaaa102234@gmail.com>
2021-07-01 18:59:52 +0800

    :bug: fixing temple bug

--- objects/temple.js
+++ objects/temple.js
@@ -1,12 +1,19 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
 import * as THREE from 'three'
 
-export function BuildTemple(scene, posX, posY, posZ) {
-  const loader = new GLTFLoader()
-  loader.load('../models/japanese_temple/scene.gltf', function (gltf) {
-    templeMesh = gltf.scene
-    templeMesh.position.set(posX, posZ, posY)
-    templeMesh.scale.set(1, 1, 1)
-    scene.add(templeMesh)
-  })
+class Temple {
+  object = new THREE.Object3D()
+
+  constructor(scene, posX, posY, posZ) {
+    const loader = new GLTFLoader()
+    loader.load('../models/japanese_temple/scene.gltf', (gltf) => {
+      this.object.add(SkeletonUtils.clone(gltf.scene))
+      this.object.position.set(posX, posY, posZ)
+      this.object.scale.set(1, 1, 1)
+      scene.add(this.object)
+    })
+  }
 }
+
+export default Temple
